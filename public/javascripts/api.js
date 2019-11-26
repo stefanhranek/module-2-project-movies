61512f01c33cce2f0f707470c111975896e94c16
stefan hranek <hranekstefan@gmail.com>
2019-11-26 21:09:50 +0100

    Movie details renders, DONE!

--- public/javascripts/api.js
+++ public/javascripts/api.js
@@ -14,39 +14,63 @@ const movieTitle = document.querySelector('.get-info');
 getMovieForm.addEventListener('submit', function(e) {
     // Prevent the form reloading of the page
     e.preventDefault();
-
+    
     // Get the id of the movie from the input field
     const name = document.querySelector('#movie-name').value;
-
+    
     // If no id was provided, stop the function
+    
     if (!name) return;
-
+    
     // Make a GET request -  get one movie
     axios
-        .get(`https://api.themoviedb.org/3/search/movie?api_key=885dbfba88f11b7023082ad1956f5310&language=en-US&query=${name}&page=1&include_adult=false`)
-        .then(response => {
+    .get(`https://api.themoviedb.org/3/search/movie?api_key=885dbfba88f11b7023082ad1956f5310&language=en-US&query=${name}&page=1&include_adult=false`)
+    .then(response => {
             // Grab all input fields from the update form (still invisible)
             // const inputNodes = updateForm.querySelectorAll('input');
             // const inputElements = [...inputNodes];
             const movies = response.data.results;
             console.log(response.data.results);
             const resultList = document.querySelector('.movie-results');
-
+            
             let htmlString = "";
             movies.forEach(function(movie) {
                 htmlString += `
-                <div class="movie-search-query">
-            <h2 class='movie-title' >${movie.title} (${movie.release_date.slice(0,4)})</h2>
-                <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}"/>
-                </div>`
+                <div>
+                        <h2 
+                        class='movie-title movie-details-link' 
+                        data-movieid='${movie.id}'
+                        >
+                        ${movie.title} (${movie.release_date.slice(0,4)})
+                        </h2>
+                        <img 
+                        class="movie-details-link" 
+                        src="https://image.tmdb.org/t/p/w500${movie.poster_path}"
+                            data-movieid='${movie.id}'
+                            />
+                            </div>`
+                            
+                        })
+                        resultList.innerHTML = htmlString;
+                        
+                        const getMovieDetailLinks = document.querySelectorAll('.movie-details-link')
 
-            })
-            console.log("THIS IS THE STRING", htmlString);
-            resultList.innerHTML = htmlString;
+                        
+getMovieDetailLinks.forEach( (element) => {
+    
+    element.addEventListener('click', (e) => {
+        const movieId = element.dataset.movieid;
+        console.log('THI IS THE MOVIEID',movieId);
+        
+        // axios.get(`/private/movieDetail/get/${movieId}`)
+        window.location.assign(`/private/movieDetail/${movieId}`)
+    } )
 
-            console.log('INPUT ELEMENTS ARE', inputElements);
 
-            // Populate each empty `inputField.value` with the 
+} )
+                        
+                        
+                        // Populate each empty `inputField.value` with the 
             // corresponding data from the response object `movie.data`
             inputElements.forEach(
                 inputField => (inputField.value = movie.data[inputField.name]),
@@ -55,6 +79,7 @@ getMovieForm.addEventListener('submit', function(e) {
         })
         .catch(err => console.log(err));
 });
+
 
 
 
@@ -100,10 +125,4 @@ addMovie: function(req, res) {
 
 
 */
-random.addEventListener('click', function() {
 
-
-    axios.get()
-        .then(response => console.log('randooom'))
-
-})
--- routes/private/movieDetail.js
+++ routes/private/movieDetail.js
@@ -1,10 +1,43 @@
 var express = require('express');
 var router  = express.Router();
+var axios   = require('axios');
+
+
+// // GET '/private/movieDetail/get'
+// router.get('/get/:movieId', function(req, res, next) {
+
+//   const { movieId } = req.params;
+
+//   res.redirect('/private/movieDetail/' + movieId)
+  
+// });
+
+
+
 
 // GET '/private/movieDetail'
-router.get('/', function(req, res, next) {
-  res.render('movieDetail');
+router.get('/:movieId', function(req, res, next) {
+
+  const { movieId } = req.params
+
+  console.log('param is ', movieId);
+
+  axios
+  .get(`https://api.themoviedb.org/3/movie/${movieId}?api_key=885dbfba88f11b7023082ad1956f5310&language=en-US`)
+  .then( (response) => {
+
+    const movie = response.data;
+
+    console.log('BLAAAA', response.data)
+    res.render('movieDetail', { movie });
+  })
+  .catch( (err) => console.log(err));
+  
 });
+
+
+
+
 
 
 module.exports = router;
