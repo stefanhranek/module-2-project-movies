8a70b8d9a6bef2993be34e60c2cbe4d11dc327f6
stefan hranek <hranekstefan@gmail.com>
2019-11-27 02:06:42 +0100

    Major style for navbar & search page

--- routes/private/searchResults.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router  = express.Router();
-
-// GET '/private/searchResults'
-router.get('/', function(req, res, next) {
-  res.render('searchResults');
-});
-
-module.exports = router;
